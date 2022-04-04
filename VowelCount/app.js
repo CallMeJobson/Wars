f7bbb665ab3324fbd8698275d361f63981d52554
Johnsonj91 <Jordan33111@gmail.com>
2022-04-03 22:32:55 -0400

    first commit

---
+++ VowelCount/app.js
@@ -0,0 +1,23 @@
+function getCount(str) {
+    var vowelsCount = 0;
+    
+    // enter your majic here
+    for(let char of str){
+      if(char === "a"){
+        vowelsCount += 1;
+      }
+      if(char === "e"){
+        vowelsCount += 1;
+      }
+      if(char === "i"){
+        vowelsCount += 1;
+      }
+      if(char === "o"){
+        vowelsCount += 1;
+      }
+      if(char === "u"){
+        vowelsCount += 1;
+      }
+    }
+    return vowelsCount;
+  }
