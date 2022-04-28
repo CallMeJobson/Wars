37a57aee50d54a03982a58a8fba3b950fa300420
Johnsonj91 <Jordan33111@gmail.com>
2022-04-28 17:26:58 -0400

    removed break statements

--- basic-math/app.js
+++ basic-math/app.js
@@ -5,15 +5,11 @@ function basicOp(operation, value1, value2)
   switch(operation){
       case "+":
       return value1 + value2
-      break;
-       case "-":
+      case "-":
       return value1 - value2
-      break;
-       case "*":
+      case "*":
       return value1 * value2
-      break;
-       case "/":
+      case "/":
       return value1 / value2
-      break;
   }
 }
