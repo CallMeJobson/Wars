abb0ee314d5969f0ca2b5035049fba1c676debdf
Johnsonj91 <Jordan33111@gmail.com>
2022-05-10 15:05:33 -0400

    Learning React

--- react/app.js
+++ react/app.js
@@ -1,1 +1,12 @@
 ReactDOM.render(<h1>All you base are belong to US!</h1>, document.getElementById('root'));
+
+
+
+function MainContent() {
+    return (
+        <h1>Im learning React</h1>
+    )
+}
+
+ReactDOM.render(<div><MainContent/></div>, document.getElementById('root'));
+
