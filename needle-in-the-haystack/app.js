2875c390963f128442d747ad26b6e2dc64322eda
Johnsonj91 <Jordan33111@gmail.com>
2022-05-11 17:32:07 -0400

    needle in the haystack problem

---
+++ needle-in-the-haystack/app.js
@@ -0,0 +1,9 @@
+function findNeedle(haystack) {
+    // your code here
+    if(haystack.includes("needle")){
+      return "found the needle at position " + haystack.indexOf("needle")
+    }
+    else{
+      return ;
+    }
+  }
