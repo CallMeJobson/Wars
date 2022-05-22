84359c5e37f0b482049eb7ce4c64f7d006bf0547
Johnsonj91 <Jordan33111@gmail.com>
2022-05-22 10:12:31 -0400

    Square root and is he going to survive problems

---
+++ 8-kyu/tosquare-ortonot/app.js
@@ -0,0 +1,14 @@
+function squareOrSquareRoot(array) {
+  
+    let result = []
+    array.forEach((x) =>{
+      if(Number.isInteger(Math.sqrt(x))){
+        result.push(Math.sqrt(x))
+      }
+      else{
+        result.push(x * x)
+      }
+    })
+    
+    return result
+  }
