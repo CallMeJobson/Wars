1188e90e59231a390f9ae65dac07010484ae9d1a
Johnsonj91 <Jordan33111@gmail.com>
2022-05-23 06:36:57 -0400

    Freudian transltor

---
+++ 8-kyu/freudian-translator/app.js
@@ -0,0 +1,12 @@
+function toFreud(string) {
+    if(string === ""){
+      return"";
+    }
+   let result = ""
+   let count = string.split(" ").length
+   
+   for(let x = 0; x < count; x++){
+      result += " sex"
+    }
+  return result.trim();
+  }
