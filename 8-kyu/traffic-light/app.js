f7694cebb34e5dc6176f764f6b6284d60feb5c6d
Johnsonj91 <Jordan33111@gmail.com>
2022-07-04 16:27:45 -0400

    traffice light problem

---
+++ 8-kyu/traffic-light/app.js
@@ -0,0 +1,12 @@
+function updateLight(current) {
+  
+    //your code here!
+    if(current === "green"){
+      return "yellow"
+    }else if(current === "red"){
+      return "green"
+    }
+    
+    return "red"
+  
+  }
