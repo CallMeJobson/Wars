53df3fb5971a64e0715a6c8add51d6feb1658970
Johnsonj91 <Jordan33111@gmail.com>
2022-07-19 17:57:59 -0400

    code wars

---
+++ 8-kyu/max-headroom/app.js
@@ -0,0 +1,14 @@
+function getMax1()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{
+    //Return the object on the same line. 
+  return{name: 'Max Headroom'}
+}
